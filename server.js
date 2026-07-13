cd3cd6915b390726ecc7a0babdac7ed853fd7ccc
Zahoor Ahmed <zahoorjamali32@gmail.com>
2026-07-14 00:02:43 +0500

    edit server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("PORT:", process.env.PORT);
 console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+console.log("TEST:", process.env.TEST);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
